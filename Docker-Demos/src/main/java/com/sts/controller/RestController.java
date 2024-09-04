ceae5f6f7e9ba00798caeb6c472c05f50dc5730c
dkawale499 <dkawale499@gmail.com>
2024-09-04 20:16:27 +0530

    project added

--- Docker-Demos/src/main/java/com/sts/controller/RestController.java
+++ Docker-Demos/src/main/java/com/sts/controller/RestController.java
@@ -1,5 +1,15 @@
 package com.sts.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+
 public class RestController {
 
+	@GetMapping("/docker")
+	public String getShow() {
+		
+		return "Docker demo sucess";
+	}
+	
 }
